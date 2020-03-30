92f1ce26c4b779236c3278f4f85dc8c64a321a64
Paulius77777 <Paulius.Prudnikovas1@gmail.com>
2020-03-30 20:14:48 +0300

    Services Done

--- js/data.js
+++ js/data.js
@@ -1,1 +1,34 @@
 "Use strict"
+
+const services = [
+    {
+        icon: 'facebook',
+        title: 'interFace Design',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'twitter',
+        title: 'Experience Design',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'instagram',
+        title: 'Web Development',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'linkedin',
+        title: 'Mobile app',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'facebook',
+        title: 'Digital marketing',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'facebook',
+        title: 'Woocommerce',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    }
+];
